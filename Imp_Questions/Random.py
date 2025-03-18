2f5594d3843b1c92e9b081505fdf9ed19ef11161
Vineet Vikram Singh <90778443+VineGamingYT@users.noreply.github.com>
2025-03-18 09:08:27 +0530

    Delete Imp_Questions/Random.py

--- Imp_Questions/Random.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter number of rows: "))
-for i in range(n, 0, -1):
-    for j in range(n - i):
-        print(" ", end="")
-    for k in range(i):
-        print("*", end="")
-    print()
